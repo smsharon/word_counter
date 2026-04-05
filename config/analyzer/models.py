c6cf1fb7f6dca6d37647ff5e1f24dc6342d73f5d
Smsharon <shallonmokeira@gmail.com>
2026-04-06 01:38:18 +0300

    create models

--- config/analyzer/models.py
+++ config/analyzer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class TextAnalysis(models.Model):
+    text = models.TextField()
+    word_count = models.IntegerField()
+    character_count = models.IntegerField()
+    sentence_count = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Analysis {self.id}"
